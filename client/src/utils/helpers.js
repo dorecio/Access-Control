76d463fe559c53ccf292d0067bd0fd34366ef19d
Julio C. MW11 <sysa.mail@gmail.com>
2023-03-08 22:08:11 -0600

    se refactoriza client, rutas en app y helper de correo, y mutation

---
+++ client/src/utils/helpers.js
@@ -0,0 +1,28 @@
+module.exports = {
+    body_email : (email, sub, visitante, motivo, idVisita) =>{
+        const message = `
+        <h3>Solicitud de acceso de : ${visitante} <br>
+        <h3>Motivo : ${motivo} <br>
+        
+        <p>Permitir: <a href='https://www.npmjs.com/visitpage?idvisita=${idVisita}&acceso=1'>PERMITIR</a></p>
+        <p>Denegar: <a href='https://www.npmjs.com/visitpage?idvisita=${idVisita}&acceso=2'>DENEGAR</a></p>
+
+        <br>
+        <p style="font-size:8pt;line-height:10pt;font-family:&#39;Cambria&#39;,&#39;times roman&#39;,serif">
+        El presente mensaje es confidencial, dirigido únicamente para el destinatario. Si usted no es el destinatario, no deberá copiarlo, revelarlo o distribuirlo. Cualquier acción realizada en este sentido, será ilegal. Si por error recibe el presente mensaje, deberá
+        destruirlo y notificar al remitente. 
+        This communication is confidential and intended only for the addressee. If you are not the intended recipient, you may not copy, disclose, or distribute this message to anyone else; any such actions may be unlawful. If
+        you have received this communication in error, you must destroy this message and contact the sender of the message to inform him or her of the error.
+        </p>        
+
+        `
+        const payload = JSON.stringify(
+                    {
+                    "email":email,
+                    "sub":sub,
+                    "msg":message
+                    }        
+                    );
+        return payload;
+    }
+};
