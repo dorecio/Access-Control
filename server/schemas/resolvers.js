5abc94cf8ba88f916a7e201551b538100236c2de
dorecio <dorisrecio@gmail.com>
2023-03-11 22:03:46 -0600

    se modifica archivo resolvers para soportar cambios de TypeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,49 +1,47 @@
 //const { Empleado, Visitante } = require('../models');
-const { Empleado, Visita } = require('../models');
-const startOfDay = require( 'date-fns/startOfDay');
-const endOfDay = require('date-fns/endOfDay') ;
+const { Empleado, Visita } = require("../models");
 
 const resolvers = {
   Query: {
     empleados: async () => {
       return await Empleado.find({});
     },
-    empleado: async (parent,{email}) => {
-      return await Empleado.findOne({email:email});
+    empleado: async (parent, { email }) => {
+      return await Empleado.findOne({ email: email });
     },
     visitasAll: async () => {
       return await Visita.find({});
     },
-    visitasGet : async () => {
+    visitasGet: async () => {
       return await Visita.find({
         //createdAt : {$gte: startOfDay(new Date()) , $lte: endOfDay(new Date()) }
       });
-    }
-/*     visitasAll: async () => {
+    },
+    /*     visitasAll: async () => {
       return await Visitante.find({});
     } */
 
-   // visitas: async (parent, { _id }) => {
-     // const params = _id ? { _id } : {};
-     // return await Visitante.find(v => (v.id === _id));
-   // },
+    // visitas: async (parent, { _id }) => {
+    // const params = _id ? { _id } : {};
+    // return await Visitante.find(v => (v.id === _id));
+    // },
   },
 
   Mutation: {
-    addEmpleado : async (parent, args) => {
+    addEmpleado: async (parent, args) => {
       return await Empleado.create(args);
     },
-    addVisita : async (parent, {email, visitante, motivo}) => {
-      return await Visita.create({email, visitante, motivo});
-  },
-  updateVisita : async (parent, {id, acceso}) => {
-    return await Visita.findOneAndUpdate(
-     {_id:id},
-     {acceso},
-     {new:true}
-   ); 
- }      
-/*     addVisita: async (parent, { args }) => {
+    addVisita: async (parent, { email, visitante, motivo }) => {
+      return await Visita.create({ email, visitante, motivo });
+    },
+    updateVisita: async (parent, { id, acceso }) => {
+      return await Visita.findOneAndUpdate(
+        { _id: id },
+        { acceso },
+        { new: true }
+      );
+    },
+    /*     addVisita: async (parent, { args }) => {
       return await Visitante.create(args);
     } */
   },
