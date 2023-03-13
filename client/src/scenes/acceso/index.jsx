01f9ccc0c283a2318259c9dfe753d4e54605d0ad
David Morales <david_morales95@outlook.com>
2023-03-12 22:49:47 -0600

    se agrega escena de acceso

--- client/src/scenes/acceso/index.jsx
+++ client/src/scenes/acceso/index.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, useTheme, useMediaQuery } from "@mui/material";
+import { useGetAgregar } from "../../state/api";
+import Header from "../../components/Header";
+import { useParams } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { body_email } from "../../utils/helpers";
+import { ADD_VISITA } from "../../utils/mutations";
+import { ADD_EMPLEADO } from "../../utils/mutations";
+import EmpleadoForm from "../../components/EmpleadoForm";
+import EmpleadoList from "../../components/EmpleadoList";
+import { QUERY_EMPLEADOS } from "../../utils/queries";
+import { useQuery } from "@apollo/client";
 
-function Acceso() {
-  return <div>Acceso</div>;
-}
+const Acceso = () => {
+  const { loading, data } = useQuery(QUERY_EMPLEADOS);
+  const empleados = data?.empleados || [];
+
+  return (
+    <div>
+      <Box>
+        <Header title="Acceso" subtitle="Solicitud de acceso" />
+        <Box>
+          <EmpleadoForm />
+        </Box>
+        <Box>
+          <div className="col-12 col-md-8 mb-3">
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <EmpleadoList
+                empleados={empleados}
+                title="Lista de Empleados..."
+              />
+            )}
+          </div>
+        </Box>
+      </Box>
+    </div>
+  );
+};
 
 export default Acceso;
