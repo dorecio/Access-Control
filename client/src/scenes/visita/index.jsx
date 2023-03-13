9eb840686561fd1e7dc84f3c271ba8a618cab40e
David Morales <david_morales95@outlook.com>
2023-03-12 23:00:30 -0600

    se genera escena de visita

--- client/src/scenes/visita/index.jsx
+++ client/src/scenes/visita/index.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, useTheme, useMediaQuery } from "@mui/material";
+import Header from "../../components/Header";
+import VisitaForm from "../../components/VisitaForm";
 
-function visita() {
-  return <div>visita</div>;
+function Visita() {
+  return (
+    <div>
+      <Box>
+        <Header title="Visita" subtitle="Solicitud de Visita" />
+      </Box>
+      <Box>
+        <VisitaForm></VisitaForm>
+      </Box>
+    </div>
+  );
 }
-
-export default visita;
+export default Visita;
