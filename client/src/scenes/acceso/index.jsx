0524531a55905f253aa0dde82282c6e2705ab750
David Morales <david_morales95@outlook.com>
2023-03-12 22:45:18 -0600

    se agrega estructura base de escenas

---
+++ client/src/scenes/acceso/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Acceso() {
+  return <div>Acceso</div>;
+}
+
+export default Acceso;
---
+++ client/src/scenes/visita/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function visita() {
+  return <div>visita</div>;
+}
+
+export default visita;
