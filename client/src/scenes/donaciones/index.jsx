63257fffef34b99f3488d0cce061805260aba3af
David Morales <david_morales95@outlook.com>
2023-03-12 22:54:46 -0600

    Se agrega escena de donaciones

--- client/src/scenes/donaciones/index.jsx
+++ client/src/scenes/donaciones/index.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { useGetDonacionesQuery } from "../../state/api";
+import Header from "../../components/Header";
+import Donacion from "../../components/Donacion";
 
-function donaciones() {
-  return <div>donaciones</div>;
+function Donaciones() {
+  return (
+    <Box>
+      <Header title="DONACIONES" subtitle="Tus donaciones nos ayudan" />
+      <Box m="15px">
+        <Typography variant="h2" fontWeight="bold" sx={{ mb: "5px" }}>
+          Ayudanos con un donativo
+        </Typography>
+      </Box>
+      <Box m="15px">
+        <Typography variant="h5" sx={{ mb: "5px" }}>
+          Somos una empresa de desarrolladores independientes. Con tus
+          donaciones nos ayudas a continuar<br></br> creando contenido y
+          software libre. Utilicemos tecnología para hacer que el mundo sea un
+          mejor lugar.
+        </Typography>
+      </Box>
+      <Box sx={{ "& button": { m: 1 } }}>
+        <div>
+          <Donacion></Donacion>
+        </div>
+      </Box>
+    </Box>
+  );
 }
 
-export default donaciones;
+export default Donaciones;
